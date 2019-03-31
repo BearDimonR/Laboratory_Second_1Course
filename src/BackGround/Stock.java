57e7093a3ae53686944147ecc1d35e5d628e14e0
igorYevtushenko <48184765+igorYevtushenko@users.noreply.github.com>
2019-03-31 08:10:34 +0300

    1) change class Stock

--- src/BackGround/Stock.java
+++ src/BackGround/Stock.java
@@ -27,8 +27,7 @@ public class Stock {
 
         for (int i = 0; i < products.length; i++) {
             Product temp = new Product(products[i].getGroupProducts().toLowerCase(),
-                    products[i].getProductName().toLowerCase(),
-                    products[i].getManufacturer().toLowerCase(),
+                    products[i].getProductName().toLowerCase(), products[i].getManufacturer().toLowerCase(),
                     products[i].getPrice());
 
             products[i] = temp;
@@ -62,46 +61,59 @@ public class Stock {
         manufacturer = manufacturer.toLowerCase();
     }
 
+    private String copyWordToString(String groupOfProducts, String word, int i) {
+        for (int j = 0; j < groupOfProducts.length(); j++) {
+            if (!(products[i].getGroupProducts().length() < groupOfProducts.length()))
+                word += products[i].getGroupProducts().charAt(j);
+        }
+        return word;
+    }
+
     /**
      * метод який перевіряє чи у полі група товарів не пусто
      *
      * @param groupOfProducts
-     * @return якщо пусто то залишає arreyList цілим інакше видаляє те що не підходить
+     * @return якщо пусто то залишає arreyList цілим інакше видаляє те що не
+     *         підходить
      */
     private ArrayList<Product> findByGroupProducts(String groupOfProducts) {
         if (groupOfProducts == null || groupOfProducts == "" || groupOfProducts == "[ ]*") {
         } else {
             String word = "";
             for (int i = 0; i < products.length; i++) {
-                for (int j = 0; j < groupOfProducts.length(); j++) {
-                    word += products[i].getGroupProducts().charAt(j);
-                }
-                if (!(word.equals(groupOfProducts))) {
+//				for (int j = 0; j < groupOfProducts.length(); j++) {
+//					if (!(products[i].getGroupProducts().length() < groupOfProducts.length()))
+//						word += products[i].getGroupProducts().charAt(j);
+//				}
+                word = copyWordToString (groupOfProducts,word,i);
+                if (!(word.equals(groupOfProducts) )) {
                     arrayList.remove(products[i]);
                 }
+                word = "";
             }
         }
         return arrayList;
     }
-
     /**
      * метод який перевіряє чи у полі назви товару товарів не пусто
      *
      * @param productsName
-     * @return якщо пусто то залишає arreyList цілим інакше видаляє те що не підходить
+     * @return якщо пусто то залишає arreyList цілим інакше видаляє те що не
+     *         підходить
      */
     private ArrayList<Product> findByProductName(String productsName) {
         if (productsName == null || productsName == "" || productsName == "[ ]*") {
         } else {
-//            for(Product tovar : products){
-//                if(  ! (tovar.getProductName().equals(products))   ){
-//                    arrayList.remove(tovar);
-//                }
-//            }
+            String word = "";
             for (int i = 0; i < products.length; i++) {
+                for (int j = 0; j < productsName.length(); j++) {
+                    if (!(products[i].getProductName().length() < productsName.length()))
+                        word += products[i].getProductName().charAt(j);
+                }
                 if (!(products[i].getProductName().equals(productsName))) {
                     arrayList.remove(products[i]);
                 }
+                word = "";
             }
         }
         return arrayList;
@@ -111,15 +123,22 @@ public class Stock {
      * метод який перевіряє чи у полі назви виробника товарів не пусто
      *
      * @param manufacturer
-     * @return якщо пусто то залишає arreyList цілим інакше видаляє те що не підходить
+     * @return якщо пусто то залишає arreyList цілим інакше видаляє те що не
+     *         підходить
      */
     private ArrayList<Product> findByManufacturer(String manufacturer) {
         if (manufacturer == null || manufacturer == "" || manufacturer == "[ ]*") {
         } else {
+            String word = "";
             for (int i = 0; i < products.length; i++) {
+                for (int j = 0; j < manufacturer.length(); j++) {
+                    if (!(products[i].getManufacturer().length() < manufacturer.length()))
+                        word += products[i].getManufacturer().charAt(j);
+                }
                 if (!(products[i].getManufacturer().equals(manufacturer))) {
                     arrayList.remove(products[i]);
                 }
+                word = "";
             }
         }
         return arrayList;
@@ -153,20 +172,20 @@ public class Stock {
         return arrayList;
     }
 
-
-    // private  void findProduct(String groupOfProducts, String productName, String manufacturer, double priceFrom, double priceTo) {
+    // private void findProduct(String groupOfProducts, String productName, String
+    // manufacturer, double priceFrom, double priceTo) {
 
     public static void main(String[] args) {
         Stock s = new Stock();
         s.reduceLettersInArray();
         s.copyToArrayList(arrayList, products);
         System.out.println(arrayList + "*************");
-        s.findByGroupProducts("мол");
+        s.findByGroupProducts("молоко");
         System.out.println(arrayList + "++++++++++++++");
         s.findByProductName(null);
-        System.out.println(arrayList + "--------------");
+        //System.out.println(arrayList + "--------------");
         s.findByPrice(0, 3.8);
-        System.out.println(arrayList + "//////////////");
+        //System.out.println(arrayList + "//////////////");
     }
 
 }
